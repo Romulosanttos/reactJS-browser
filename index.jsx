c18467671b3ed2a24834da360143e06fdf413b2f
romulosanttos <romulosanttos1@gmail.com>
2019-07-18 06:14:57 -0300

    terminando botão é pequenos ajustes

--- index.jsx
+++ index.jsx
@@ -14,9 +14,11 @@ const {
 	SvgIcon,
 	Link,
 
+	Button,
 	Select,
 	MenuItem,
-	Input
+	InputLabel,
+	FormControl
 } = MaterialUI;
 
 // Create a theme instance.
@@ -55,66 +57,82 @@ const useStyles = makeStyles(theme => ({
 		display: 'flex',
 		margin: '20%',
 		justifyContent: 'center'
-	},
-	root: {
-
 	}
 }));
 
-
-function Header() {
-	return (
-		<Typography align="center" variant="h4" component="h1">
-			React JS
-		</Typography>
-	);
-}
-
-
-const customStyles = {
-	container: (base, state) => ({
-		...base,
-		opacity: 0,
-		backgroundColor: "white",
-		zIndex: "999"
-	})
+const getInfoAPI = () =>{
+	return  [
+		{"nome": "Item 1", "valor": 1},
+		{"nome": "Item 2", "valor": 2},
+		{"nome": "Item 2", "valor": 2},
+		{"nome": "Item 3", "valor": 3},
+		{"nome": "Item 4", "valor": 4},
+		{"nome": "Item 5", "valor": 5},
+		{"nome": "Item 6", "valor": 6},
+		{"nome": "Item 7", "valor": 7},
+		{"nome": "Item 8", "valor": 8}
+	].filter(function (a) {
+		return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+	}, {})
 };
 
 class SimpleSelect extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {value: 'coconut'};
-
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {
+			selectOptions: getInfoAPI(),
+			selectedValue: "selecione uma alternativa"
+		};
 	}
 
-	handleChange(event) {
-		this.setState({value: event.target.value});
-	}
+	renderSelectOptions = () => {
+		return this.state.selectOptions.map((dt, i) => (
+			<MenuItem key={dt.valor} value={dt.valor}>
+				{dt.nome}
+			</MenuItem>
+		));
+	};
 
-	handleSubmit(event) {
-		alert('Your favorite flavor is: ' + this.state.value);
+	handleChange = event => {
+		this.setState({ selectedValue: event.target.value });
+	};
+
+	handleSubmit = (event) => {
 		event.preventDefault();
-	}
+
+		const data = getInfoAPI();
+		this.setState({ selectOptions: data });
+
+	};
+
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					Pick your favorite flavor:
-					<select value={this.state.value} onChange={this.handleChange}>
-						<option value="grapefruit">Grapefruit</option>
-						<option value="lime">Lime</option>
-						<option value="coconut">Coconut</option>
-						<option value="mango">Mango</option>
-					</select>
-				</label>
-				<input type="submit" value="Submit" />
+			<form autoComplete="off" onSubmit={this.handleSubmit} >
+				<FormControl style={{width: '25vh',background: '#FFF'}}>
+					<InputLabel style={{background: 'gray',fontSize: 16}} htmlFor="items">Items</InputLabel>
+					<Select style={{width: '25vh',background: '#FFF'}} inputProps={{
+							id: 'items',
+					}} value={this.state.selectedValue} onChange={this.handleChange}>
+							{this.renderSelectOptions()}
+					</Select>
+					<Button type="submit" variant="contained" color="primary" >
+						Forçar atualização
+					</Button>
+				</FormControl>
 			</form>
 		);
 	}
 }
-function Footer() {
+
+const Header = ()=> {
+	return (
+		<Typography align="center" variant="h4" component="h1">
+			React JS
+		</Typography>
+	);
+};
+
+const Footer = () =>  {
 	return (
 		<Typography  align="center">
 			{'Copyright © 2019  '}
@@ -123,22 +141,21 @@ function Footer() {
 			</Link>
 		</Typography>
 	);
-}
+};
 
-function Body() {
+const Body = () => {
 	const classes = useStyles();
 	return (
 		<Typography component={'span'} variant={'body2'} className={classes.body}>
 			<SimpleSelect/>
 		</Typography>
 	);
-}
+};
 
 
-function App() {
+const App = ()=> {
 	const classes = useStyles();
 	return (
-
 		<MuiThemeProvider theme={theme} >
 			<CssBaseline />
 			<div className={classes.root} >
@@ -158,12 +175,9 @@ function App() {
 					</Toolbar>
 				</AppBar>
 			</div>
-
 		</MuiThemeProvider>
-
-
 	);
-}
+};
 
 ReactDOM.render(
 	<App/>,
